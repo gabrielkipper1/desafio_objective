b741890504af808a9db2a94be13bf77a86a5d4cb
Gabriel Limberger <g.kipper@hotmail.com>
2024-04-14 18:13:30 -0300

    add generic type to character repository
    -include the type "Character" to the "MarvelApiRequestData" generic object to have an explicit return type and avoid a cast from a dynamic type

--- lib/src/repositories/character_repository/character_repository.dart
+++ lib/src/repositories/character_repository/character_repository.dart
@@ -1,6 +1,7 @@
+import 'package:desafio_objective/src/entities/character.dart';
 import 'package:desafio_objective/src/entities/generic/marvel_api_request_data.dart';
 
 abstract class CharacterRepository {
-  Future<MarvelApiRequestData> getCharacters();
-  Future<MarvelApiRequestData> searchCharacters({String? search});
+  Future<MarvelApiRequestData<Character>> getCharacters({int? offset, int? limit});
+  Future<MarvelApiRequestData<Character>> searchCharacters({String? search, int? offset, int? limit});
 }
