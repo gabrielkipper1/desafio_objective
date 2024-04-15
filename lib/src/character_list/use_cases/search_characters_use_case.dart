224cc3bee23f215533141dd21eb63b4074cec5b5
Gabriel Limberger <g.kipper@hotmail.com>
2024-04-14 22:46:22 -0300

    create "change page" method for use search character use case to avoid depending on all of the view components to make pagination updates.
    - the use case now keeps a reference to the current search and page.

--- lib/src/character_list/use_cases/search_characters_use_case.dart
+++ lib/src/character_list/use_cases/search_characters_use_case.dart
@@ -4,12 +4,25 @@ import 'package:desafio_objective/src/repositories/character_repository/characte
 
 class SearchCharactersUseCase {
   final CharacterRepository _characterRepository;
-
   SearchCharactersUseCase(this._characterRepository);
 
+  String search = "";
+  int page = 0;
+
   Future<MarvelApiRequestData<Character>> searchCharacters({String? query, required int offset, required int limit}) async {
+    search = query ?? "";
+    page = (offset / limit).floor();
+
     try {
       return await _characterRepository.searchCharacters(search: query, offset: offset, limit: limit);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<MarvelApiRequestData<Character>> changePage({required int page, required int limit}) async {
+    try {
+      return await _characterRepository.searchCharacters(search: search, offset: page * limit, limit: limit);
     } catch (e) {
       rethrow;
     }
