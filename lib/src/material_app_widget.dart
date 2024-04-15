3cda62f86c25efebef722ab5690c8fe1ea08aeee
Gabriel Limberger <g.kipper@hotmail.com>
2024-04-15 12:35:30 -0300

    update material app widget to include responsive_framework
    - add responsiveness support by using breakpoints
    - change color scheme to red
    - change material app title

--- lib/src/material_app_widget.dart
+++ lib/src/material_app_widget.dart
@@ -1,5 +1,6 @@
-import 'package:desafio_objective/src/character_list/character_list_screen.dart';
+import 'package:desafio_objective/src/character_list/pages/character_list_page.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class MaterialAppWidget extends StatelessWidget {
   const MaterialAppWidget({super.key});
@@ -7,12 +8,19 @@ class MaterialAppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Marvel Characters',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: const CharacterListScreen(),
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        breakpoints: [
+          const Breakpoint(start: 0, name: MOBILE, end: 600),
+          const Breakpoint(start: 600, name: DESKTOP, end: 1024),
+        ],
+        child: child!,
+      ),
+      home: const CharacterListPage(),
     );
   }
 }
