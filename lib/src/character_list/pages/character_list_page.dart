743143010c7b1abe24f2a35f17940b4bfc0e7bfa
Gabriel Limberger <g.kipper@hotmail.com>
2024-04-15 12:31:53 -0300

    update character list page
    - move the file to another folder and rename it to "character_list_page.dart"
    -include search bar, list builder and paginator
    -include tests to check if everything is rendered

---
+++ lib/src/character_list/pages/character_list_page.dart
@@ -0,0 +1,54 @@
+import 'package:desafio_objective/src/character_list/bloc/character_list_bloc.dart';
+import 'package:desafio_objective/src/character_list/widgets/character_list_widgets/character_list_builder.dart';
+import 'package:desafio_objective/src/character_list/widgets/character_list_paginator.dart';
+import 'package:desafio_objective/src/character_list/widgets/character_list_search_bar.dart';
+import 'package:desafio_objective/src/interceptors/dio/marvel_auth_interceptor_dio.dart';
+import 'package:desafio_objective/src/repositories/character_repository/character_repository.dart';
+import 'package:desafio_objective/src/repositories/character_repository/marvel_character_service.dart';
+import 'package:desafio_objective/src/repositories/http_client/dio_http.dart';
+import 'package:desafio_objective/src/repositories/http_client/http_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CharacterListPage extends StatefulWidget {
+  const CharacterListPage({super.key});
+
+  @override
+  State<CharacterListPage> createState() => _CharacterListPageState();
+}
+
+class _CharacterListPageState extends State<CharacterListPage> {
+  final HttpRepository http = DioHttp(interceptors: [MarvelAuthInterceptorDio()]);
+  late CharacterRepository characterRepository;
+  late CharacterListBloc _bloc;
+
+  @override
+  void initState() {
+    characterRepository = MarvelCharacterService(httpClient: http);
+    _bloc = CharacterListBloc(characterRepository: characterRepository);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider.value(
+      value: _bloc,
+      child: BlocListener<CharacterListBloc, CharacterListState>(
+        listener: (context, state) {},
+        child: Scaffold(
+          resizeToAvoidBottomInset: true,
+          appBar: AppBar(title: const Text("Character List")),
+          body: const Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CharacterListSearchBar(),
+              Expanded(child: CharacterListBuilder()),
+              CharacterListPaginator(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
