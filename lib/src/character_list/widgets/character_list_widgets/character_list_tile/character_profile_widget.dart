701331395f2bb1ae1de8d1c0a7beee124864add4
Gabriel Limberger <g.kipper@hotmail.com>
2024-04-15 01:00:06 -0300

    create character profile widget to display name and photo
    -takes a character as input and displays name and photo
    -add network_image_mock to test http images

---
+++ lib/src/character_list/widgets/character_list_widgets/character_list_tile/character_profile_widget.dart
@@ -0,0 +1,34 @@
+import 'package:desafio_objective/src/entities/character.dart';
+import 'package:flutter/material.dart';
+
+class CharacterProfileWidget extends StatelessWidget {
+  final Character character;
+  const CharacterProfileWidget({super.key, required this.character});
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: ListTile(
+          title: Text(
+            character.name,
+            key: const Key("character_name"),
+          ),
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(50.0),
+            child: Image.network(
+              key: const Key("character_image"),
+              width: 58,
+              height: 58,
+              fit: BoxFit.contain,
+              scale: 1,
+              character.thumbnail.fullPath,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ test/src/character_list/widgets/character_list_widgets/character_list_tile/character_profile_widget_test.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:desafio_objective/src/character_list/widgets/character_list_widgets/character_list_tile/character_name_widget.dart';
+import 'package:desafio_objective/src/entities/character.dart';
+import 'package:desafio_objective/src/entities/generic/marvel_api_request_data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:network_image_mock/network_image_mock.dart';
+
+import '../../../../sample_data/testingSampleData.dart';
+
+void main() {
+  group("Character Name Widget", () {
+    late Character character;
+    late Widget widget;
+
+    setUp(() {
+      Map<String, dynamic> json = jsonDecode(testingSampleData("/json/characters.json"));
+      MarvelApiRequestData<Character> request = MarvelApiRequestData<Character>.fromJson(
+        json["data"],
+        (characterJson) => Character.fromJson(characterJson),
+      );
+      character = request.results.first;
+
+      widget = Material(
+        child: CharacterProfileWidget(
+          character: character,
+        ),
+      );
+    });
+
+    testWidgets("should have a character name text", (tester) async {
+      await mockNetworkImagesFor(() async {
+        await tester.pumpWidget(widget);
+      });
+      final characterName = find.byKey(const Key("character_name"));
+      final textWidget = tester.widget<Text>(characterName);
+      expect(characterName, findsOneWidget);
+      expect(textWidget.data, character.name);
+    });
+
+    testWidgets("should have a character image widget", (widgetTester) async {
+      await mockNetworkImagesFor(() async {
+        await widgetTester.pumpWidget(widget);
+      });
+      final characterPhoto = find.byKey(const Key("character_image"));
+      expect(characterPhoto, findsOneWidget);
+    });
+  });
+}
