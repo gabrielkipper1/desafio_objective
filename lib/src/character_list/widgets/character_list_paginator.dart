15e2c4c6011fe99e73df8cc7156e8bf83a62d83e
Gabriel Limberger <g.kipper@hotmail.com>
2024-04-15 09:25:13 -0300

    create list paginator widget
    - using package "number_paginator"
    - only completely rebuild when there is a new search
    - does not appear when there is not elements on the list

---
+++ lib/src/character_list/widgets/character_list_paginator.dart
@@ -0,0 +1,54 @@
+import 'dart:math';
+import 'package:desafio_objective/src/character_list/bloc/character_list_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:number_paginator/number_paginator.dart';
+import 'package:provider/provider.dart';
+
+class CharacterListPaginator extends StatefulWidget {
+  final Function(int)? onPageChanged;
+  const CharacterListPaginator({this.onPageChanged, super.key});
+
+  @override
+  State<CharacterListPaginator> createState() => _CharacterListPaginatorState();
+}
+
+class _CharacterListPaginatorState extends State<CharacterListPaginator> {
+  final NumberPaginatorController _paginatorController = NumberPaginatorController();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CharacterListBloc, CharacterListState>(
+      buildWhen: (previous, current) => previous is CharacterListLoaded || current is CharacterListLoading,
+      builder: (context, state) {
+        if (state is CharacterListLoaded) {
+          if (state.characters.results.isEmpty) return const SizedBox();
+
+          int currentPage = max((state.characters.offset / 4).ceil(), 0);
+          int totalPages = max((state.characters.total / 4).ceil(), 1);
+
+          return SizedBox(
+            width: 600,
+            child: NumberPaginator(
+              key: const Key("paginator"),
+              numberPages: totalPages,
+              onPageChange: (int page) => onPageChanged(page),
+              initialPage: currentPage,
+              controller: _paginatorController,
+            ),
+          );
+        } else {
+          return const SizedBox();
+        }
+      },
+    );
+  }
+
+  void onPageChanged(int page) {
+    if (!mounted) return;
+    setState(() {});
+    Provider.of<CharacterListBloc>(context, listen: false).add(ChangePageEvent(page: page, limit: 4));
+
+    // widget.onPageChanged?.call(page);
+  }
+}
