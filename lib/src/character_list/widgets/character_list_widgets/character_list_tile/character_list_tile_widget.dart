2a15bafe6cef8bee9b53665cb8aa95cd6e593f28
Gabriel Limberger <g.kipper@hotmail.com>
2024-04-15 09:30:21 -0300

    create character list tile widget
    -works like a builder
    -renders the widget based on the screen size

---
+++ lib/src/character_list/widgets/character_list_widgets/character_list_tile/character_list_tile_widget.dart
@@ -0,0 +1,20 @@
+import 'package:desafio_objective/src/character_list/widgets/character_list_widgets/character_list_tile/character_list_tile_landscape.dart';
+import 'package:desafio_objective/src/character_list/widgets/character_list_widgets/character_list_tile/character_profile_widget.dart';
+import 'package:desafio_objective/src/entities/character.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+class CharacterListTileWidget extends StatelessWidget {
+  final Character character;
+  const CharacterListTileWidget({super.key, required this.character});
+
+  @override
+  Widget build(BuildContext context) {
+    final breakpoint = ResponsiveBreakpoints.of(context).breakpoint;
+    if (breakpoint.name == MOBILE) {
+      return CharacterProfileWidget(character: character);
+    } else {
+      return CharacterListTileLandscapeWidget(character: character);
+    }
+  }
+}
