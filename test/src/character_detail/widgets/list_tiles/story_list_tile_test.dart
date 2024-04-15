40332ce4dd5fa2f32c23a8b06ca7b0377220a75e
Gabriel Limberger <g.kipper@hotmail.com>
2024-04-15 17:44:31 -0300

    create story list tile
    - render story type and story name
    - include test to check if everything is being rendered

---
+++ test/src/character_detail/widgets/list_tiles/story_list_tile_test.dart
@@ -0,0 +1,15 @@
+import 'package:desafio_objective/src/character_detail/widgets/list_tiles/story_list_tile.dart';
+import 'package:desafio_objective/src/entities/story.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("Story List Tile", () {
+    testWidgets("Should show the story name", (tester) async {
+      Story story = Story(name: "Story Name", type: "Story Type", resourceURI: "http://story.com");
+      await tester.pumpWidget(Material(child: StoryListTile(story: story)));
+      expect(find.text("Story Name"), findsOneWidget);
+      expect(find.text("Story Type"), findsOneWidget);
+    });
+  });
+}
